758f20dd839e008fe8873738a04c8f26295d86c8
Sarah Holder <sr.holder@yahoo.com>
2020-07-22 14:33:35 -0500

    updated arboretum class to include lists for all biomes

--- arboretum.py
+++ arboretum.py
@@ -2,5 +2,52 @@ class Arboretum:
     def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.rivers = []
-        self.grasslands = []
+        self.__mountains = []
+        self.__swamps = []
+        self.__grasslands = []
+        self.__forests = []
+        self.__rivers = []
+        self.__coastlines = []
+
+    @property
+    def mountains(self):
+        return self.__mountains
+
+    def annex_mountain(self, mountain):
+        self.__mountains.append(mountain)
+
+    @property
+    def swamps(self):
+        return self.__swamps
+
+    def annex_swamp(self, swamp):
+        self.__swamps.append(swamp)
+
+    @property
+    def grasslands(self):
+        return self.__grasslands
+
+    def annex_grassland(self, grassland):
+        self.__grasslands.append(grassland)
+
+    @property
+    def forests(self):
+        return self.__forests
+
+    def annex_forest(self, forest):
+        self.__forests.append(forest)
+
+    @property
+    def coastlines(self):
+        return self.__coastlines
+
+    def annex_coastline(self, coastline):
+        self.__coastlines.append(coastline)
+ 
+    @property
+    def rivers(self):
+        return self.__rivers
+
+    def annex_river(self, river):
+        self.__rivers.append(river)
+
