7531f0b5204b4c6e282eb6b143e5fb5eb84485c5
Kirk Sudduth <kirksudduth@gmail.com>
2020-07-23 15:47:08 -0500

    updating stuff

--- actions/report.py
+++ actions/report.py
@@ -1,10 +1,14 @@
 def build_facility_report(arboretum):
     for river in arboretum.rivers:
         print(f'{river.name} - {river.id}')
+        for animal in river.animals:
+            print(f'ANIMAL: {animal.species} - {animal.id}')
         print(
-            f"This place has {len(river.animals)} animals and {len(river.plants)} plants in it")
+            f"  *{river.name} has {len(river.animals)} animals and {len(river.plants)} plants in it")
 
     for swamp in arboretum.swamps:
         print(f'{swamp.name} - {swamp.id}]')
+        print(
+            f"  *{swamp.name} has {len(swamp.animals)} animals and {len(swamp.plants)} plants in it")
 
     input("\n\nPress any key to continue...")
