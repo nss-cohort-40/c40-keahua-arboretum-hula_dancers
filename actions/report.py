9ac9c74d84381af7a40ac6c7bcf260d94ef5c659
tannerb9 <tanner-brainard@hotmail.com>
2020-07-24 11:33:28 -0500

    Corrected grammatical errors.

--- actions/report.py
+++ actions/report.py
@@ -2,31 +2,31 @@ def build_facility_report(arboretum):
     for river in arboretum.rivers:
         river_id = str(river.id)
         print(f'{river.name} - {river_id[:8]}')
-        print(f"This place has {len(river.animals)} animals in it")
+        print(f"There are {len(river.animals)} animals here.")
 
     for swamp in arboretum.swamps:
         swamp_id = str(swamp.id)
         print(f'{swamp.name} - {swamp_id[:8]}')
-        print(f"This place has {len(swamp.animals)} animals in it")
+        print(f"There are {len(swamp.animals)} animals here.")
 
     for coastline in arboretum.coastlines:
         coastline_id = str(coastline.id)
-        print(f'{coastline.name} - {coastline_id[:8]}]')
-        print(f"This place has {len(coastline.animals)} animals in it")
+        print(f'{coastline.name} - {coastline_id[:8]}')
+        print(f"There are {len(coastline.animals)} animals here.")
 
     for grassland in arboretum.grasslands:
         grassland_id = str(grassland.id)
-        print(f'{grassland.name} - {grassland_id[:8]}]')
-        print(f"This place has {len(grassland.animals)} animals in it")
+        print(f'{grassland.name} - {grassland_id[:8]}')
+        print(f"There are {len(grassland.animals)} animals here.")
 
     for forest in arboretum.forests:
         forest_id = str(forest.id)
-        print(f'{forest.name} - {forest_id[:8]}]')
-        print(f"This place has {len(forest.animals)} animals in it")
+        print(f'{forest.name} - {forest_id[:8]}')
+        print(f"There are {len(forest.animals)} animals here.")
 
     for mountain in arboretum.mountains:
         mountain_id = str(mountain.id)
-        print(f'{mountain.name} - {mountain_id[:8]}]')
-        print(f"This place has {len(mountain.animals)} animals in it")
+        print(f'{mountain.name} - {mountain_id[:8]}')
+        print(f"There are {len(mountain.animals)} animals here.")
 
     input("\n\nPress any key to continue...")
