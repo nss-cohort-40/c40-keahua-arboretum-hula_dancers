1fb34764a659329df9d00380cf29dc2562859aea
tannerb9 <tanner-brainard@hotmail.com>
2020-07-24 11:23:05 -0500

    ID now displaying 8 chars on output.

--- actions/report.py
+++ actions/report.py
@@ -1,23 +1,32 @@
 def build_facility_report(arboretum):
     for river in arboretum.rivers:
-        print(f'{river.name} - {river.id}')
+        river_id = str(river.id)
+        print(f'{river.name} - {river_id[:8]}')
         print(f"This place has {len(river.animals)} animals in it")
 
     for swamp in arboretum.swamps:
         swamp_id = str(swamp.id)
-        print(swamp_id)
-        print(f'{swamp.name} - {swamp_id[:9]}')
+        print(f'{swamp.name} - {swamp_id[:8]}')
+        print(f"This place has {len(swamp.animals)} animals in it")
 
     for coastline in arboretum.coastlines:
-        print(f'{coastline.name} - {coastline.id}]')
+        coastline_id = str(coastline.id)
+        print(f'{coastline.name} - {coastline_id[:8]}]')
+        print(f"This place has {len(coastline.animals)} animals in it")
 
     for grassland in arboretum.grasslands:
-        print(f'{grassland.name} - {grassland.id}]')
+        grassland_id = str(grassland.id)
+        print(f'{grassland.name} - {grassland_id[:8]}]')
+        print(f"This place has {len(grassland.animals)} animals in it")
 
     for forest in arboretum.forests:
-        print(f'{forest.name} - {forest.id}]')
+        forest_id = str(forest.id)
+        print(f'{forest.name} - {forest_id[:8]}]')
+        print(f"This place has {len(forest.animals)} animals in it")
 
     for mountain in arboretum.mountains:
-        print(f'{mountain.name} - {mountain.id}]')
+        mountain_id = str(mountain.id)
+        print(f'{mountain.name} - {mountain_id[:8]}]')
+        print(f"This place has {len(mountain.animals)} animals in it")
 
     input("\n\nPress any key to continue...")
