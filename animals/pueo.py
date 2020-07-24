0eda9ac864e7f61107693cdf8871456a98158851
leigharobinson <lmarobinson24@gmail.com>
2020-07-24 16:19:46 -0500

    I commented out Grasslands, Forests and fixed bug in Pueo

--- animals/pueo.py
+++ animals/pueo.py
@@ -1,15 +1,15 @@
 from animals.animal import Animal
 from identifiable import Identifiable
-from environments.grassland import Grassland
-from environments.forest import Forest
+# from environments.grassland import Grassland
+# from environments.forest import Forest
 
 
-class Pueo(Animal, Grassland, Forest, Identifiable):  # add Freshwater
+class Pueo(Animal, Identifiable):  # add Freshwater
 
     def __init__(self):
         Animal.__init__(self, "Pueo")
-        Grassland.__init__(self)
-        Forest.__init__(self)
+        # Grassland.__init__(self)
+        # Forest.__init__(self)
         Identifiable.__init__(self)
         Animal.prey = {}
         self.minimum_age_in_months = 8
