c0bc59c11c9537e7f1964a5a9a4e0612b047fdeb
Sarah Holder <sr.holder@yahoo.com>
2020-07-24 12:30:27 -0500

    added opeapea

--- animals/opeapea.py
+++ animals/opeapea.py
@@ -6,4 +6,4 @@ class Opeapea(Animal, Identifiable):
     def __init__(self):
         Animal.__init__(self, "Ope\'ape\'a")
         Identifiable.__init__(self)
-        Animal.prey = {}
+        Animal.prey = {} 
