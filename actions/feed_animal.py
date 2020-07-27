7da3778714177d69bcadad6de84c9b797ce85feb
tannerb9 <tanner-brainard@hotmail.com>
2020-07-27 09:38:51 -0500

    Created feed_animal.py.

---
+++ actions/feed_animal.py
@@ -0,0 +1,12 @@
+
+
+def feed_animal(arboretum):
+
+    print("1. River Dolphin")
+    print("2. Pueo")
+    print("3. Ulae")
+    print("4. Gold Dust Day Gecko")
+    print("5. Nene Goose")
+    print("6. Kikakapu")
+    print("7. Ope'ape'a")
+    print("8. Hawaiian Happy-Face Spider")
